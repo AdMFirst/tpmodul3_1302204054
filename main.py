aeccce1415e2c3cf05a7b472316f0959a1b2fda4
Aditya MP <211wadityaw112@gmail.com>
2022-03-05 17:05:39 +0700

    menambahkan implementasi dengan state-based construction

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 class KodePos:
+    """
+    Insert fancy comment here, jk
+    class KodePos, sesuai dengan dokumentasi yg
+    ada di soal tp
+    """
     data = {
         "Batununggal": "40266",
         "Kujangsari": "40287",
@@ -19,8 +24,36 @@ class KodePos:
         except:
             print("Kelurahan anda tidak ditemukan")
 
+class DoorMachine:
+    """
+    Insert fancy comment here, jk
+    class DoorMachine, sesuai dengan dokumentasi yg
+    ada di soal tp
+    """
+    state = "Terkunci"
+
+    def BukaPintu(self):
+        if self.state == "Terkunci":
+            self.state = "Terbuka"
+            print("Pintu tidak terkunci")
+        else:
+            pass
+    
+    def KunciPintu(self):
+        if self.state == "Terbuka":
+            self.state = "Terkunci"
+            print("Pintu terkunci")
+        else:
+            pass
+    
+    def CekKondisi(self):
+        print(self.state)
+
 if __name__ == '__main__':
     Kabupaten = KodePos()
     Kabupaten.getKodePos(input("Masukan kelurahan yang dicari :"))
-    Kabupaten.getKodePos("Maleer")
-    Kabupaten.getKodePos("Bojongsoang")
+
+    pintuRumah = DoorMachine()
+    pintuRumah.CekKondisi()
+    pintuRumah.BukaPintu()
+    pintuRumah.KunciPintu()
